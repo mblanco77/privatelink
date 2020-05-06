78163ed06fed99cf4da8cf18d0eabfa9a279a423
Mauricio Blanco <mblanco@microsoft.com>
2020-05-06 12:32:58 -0500

    AutoApproval Workflow

--- webappazuredns/deploy.ps1
+++ webappazuredns/deploy.ps1
@@ -6,6 +6,7 @@ $resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
 $location = Read-Host -Prompt "Enter the location (i.e. eastus)"
 $username = Read-Host -Prompt "Enter the UserName" 
 $pass = Read-Host -Prompt "Enter the Vm Password or enter to autogen" -AsSecureString
+$autoApprove = Read-Host -Prompt "private link auto Approval (yes/no)"
 
 if ([string]::IsNullOrEmpty($location)){
     $location = "eastus"
@@ -13,11 +14,14 @@ if ([string]::IsNullOrEmpty($location)){
 if ([string]::IsNullOrEmpty($username)){
     $username = "azureuser"
 }
+if ([string]::IsNullOrEmpty($autoApprove)){
+    $autoApprove = "yes"
+}
 
 New-AzResourceGroup -Name $resourceGroupName -Location "$location"
 if ($pass.Length -gt 0){
-    New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -vmUser $username -VmPass $pass -TemplateFile "./azuredeploy.json" -TemplateParameterFile "./azuredeploy.parameters.json" -Verbose
+    New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -vmUser $username -VmPass $pass -autoApproval $autoApprove -TemplateFile "./azuredeploy.json" -TemplateParameterFile "./azuredeploy.parameters.json" -Verbose
 }
 else {
-    New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -vmUser $username -TemplateFile "./azuredeploy.json" -TemplateParameterFile "./azuredeploy.parameters.json" -Verbose
+    New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -vmUser $username -autoApproval $autoApprove -TemplateFile "./azuredeploy.json" -TemplateParameterFile "./azuredeploy.parameters.json" -Verbose
 }
